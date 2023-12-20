ea2f13c7356788e2137f3916d97dfa064e381e39
martiles1 <marartiles2@gmail.com>
2023-12-20 10:31:02 +0100

    End of section 2

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using API.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DataContext>(opt => 
+{
+        opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
